b4a59339ca33c23d1717346cb983fa7bd00c9f12
linewelder <redalerm@gmail.com>
2023-12-07 05:21:29 +0100

    Remove printf

--- kernel/include/kernel/printf.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdarg.h>
-
-/** Supports only %d %x %c %s formatters without precision and width. */
-int vprintf(const char* format, va_list args);
-
-/** Supports only %d %x %c %s formatters without precision and width. */
-int printf(const char* format, ...) __attribute__((format(printf, 1, 2)));
--- kernel/kernel/print.cpp
+++ kernel/kernel/print.cpp
@@ -1,21 +1,52 @@
 #include <kernel/print.h>
 
-#include <kernel/printf.h>
+#include <arch/i386/terminal.h>
+
+static void print_number(int value, int base) {
+    if (value == 0) {
+        terminal::putchar('0');
+    }
+
+    if (value < 0) {
+        terminal::putchar('-');
+        value = -value;
+    }
+
+    constexpr int BUF_SIZE = 12;
+    char buf[BUF_SIZE];
+    int buf_start = BUF_SIZE;
+
+    while (value) {
+        buf_start--;
+        int digit = value % base;
+        if (digit < 10) {
+            buf[buf_start] = '0' + digit;
+        } else {
+            buf[buf_start] = 'a' + digit - 10;
+        }
+
+        value /= base;
+    }
+
+    for (int k = buf_start; k < BUF_SIZE; k++) {
+        terminal::putchar(buf[k]);
+    }
+}
 
 void print_value(StringView value, char) {
     for (size_t i = 0; i < value.get_size(); i++) {
-        printf("%c", value[i]);
+        terminal::putchar(value[i]);
     }
 }
 
 void print_value(const char* value, char) {
-    printf("%s", value);
+    terminal::write_cstr(value);
 }
 
 void print_value(int value, char format) {
     if (format == 'x') {
-        printf("%x", value);
+        print_number(value, 16);
     } else {
-        printf("%d", value);
+        print_number(value, 10);
     }
 }
