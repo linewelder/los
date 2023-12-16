8d1e8d78d2413f9a95981a69e9aafa65b5f7870d
linewelder <redalerm@gmail.com>
2023-12-16 02:45:34 +0100

    refactor: Make min and max deduce T from the first parameter
    Wrap the second argument's type in IdentityType to allow implicit casting in calls.

--- kernel/include/util/math.hpp
+++ kernel/include/util/math.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <util/util.hpp>
+
 template <typename T>
-inline T max(T a, T b) {
+inline T max(T a, IdentityType<T> b) {
     return a > b ? a : b;
 }
 
 template <typename T>
-inline T min(T a, T b) {
+inline T min(T a, IdentityType<T> b) {
     return a < b ? a : b;
 }
